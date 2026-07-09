0258423efe5d69163081635c07852953d1f445db
Zeeshan Haider <ZeeshanHaider.1237750@gmail.com>
2026-07-09 22:08:04 +0500

    home and contribute portion

--- src/modules/domController.js
+++ src/modules/domController.js
@@ -1,66 +1,61 @@
+import '../pages/home_page/home.css';
+import '../pages/contribute_page/contribute.css';
+
 export const homeBox = () => {
+
   const homeContainer = document.createElement("div");
+  homeContainer.className = "homeContainer";
 
   const homeHead = document.createElement("div");
   homeContainer.appendChild(homeHead);
+  homeHead.className = "homeHead";
 
   const homeBody = document.createElement("div");
   homeContainer.appendChild(homeBody);
+  homeBody.className = "homeBody";
 
   const homeFoot = document.createElement("div");
   homeContainer.appendChild(homeFoot);
+  homeFoot.className = "homeFoot";
 
   const head = document.createElement("div");
   homeHead.appendChild(head);
   head.innerHTML = "HOME";
+  head.className = "head";
 
   const headText = document.createElement("div");
   homeHead.appendChild(headText);
+  headText.className = "headText"
   headText.innerHTML =
     "This Site allows you to enlist your Todo, recycle and reuse them. I means, make your time easy by mapping out, what you have to do. <br> Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quo tenetur, ad dicta voluptates minus cum vel rem libero! Sed tenetur molestiae ad repellat nam iste in sit beatae recusandae?";
 
   const bodyText = document.createElement("div");
   homeBody.appendChild(bodyText);
+  bodyText.className = "bodyText";
   bodyText.innerHTML =
     "This site is easy, Click on project menu, and you will see your past todo's and can add more using Add Todo button in Project menu. <br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quo tenetur, ad dicta voluptates minus cum vel rem libero! Sed tenetur molestiae ad repellat nam iste in sit beatae recusandae?";
 
   const footText = document.createElement("div");
   homeFoot.appendChild(footText);
+  footText.className = "footText";
   footText.innerHTML =
     "Everything is structured and Every Todo remains there on the same Device.<br> Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quo tenetur, ad dicta voluptates minus cum vel rem libero! Sed tenetur molestiae ad repellat nam iste in sit beatae recusandae?";
 
-  homeContainer.style.display = "flex";
-  homeContainer.style.flexDirection = "column";
-  homeContainer.style.gap = "2.5vh";
-  homeContainer.style.justifyContent = "space-around";
-  homeContainer.style.alignItems = "center";
-  homeContainer.style.padding = "4vw 4vh"
-  homeContainer.style.height = "100%"
-  homeContainer.style.color = "white";
-
-  homeHead.style.display = "flex";
-  homeHead.style.flexDirection = "column";
-  homeHead.style.justifyContent = "center";
-  homeHead.style.alignItems = "center";
-  homeHead.style.textAlign = "center"
-  homeHead.style.color = "red";
- 
-  head.style.fontSize = "2rem";
-  head.style.color = "red";
-
-  homeFoot.style.color = "yellow";
-  homeFoot.style.display = "flex";
-  homeFoot.style.flexDirection = "column";
-  homeFoot.style.alignItems = "center";
-  homeFoot.style.justifyContent = "center";
-  homeFoot.style.textAlign = "center";
-
-  headText.style.fontSize = "1.1rem";
-
-  homeBody.style.display = "flex";
-  homeBody.style.flexDirection = "column";
-  homeBody.style.justifyContent = "center";
-  homeBody.style.textAlign = "center";
-
   return homeContainer;
 };
+export const contributeBox = () => {
+  const contributeContainer = document.createElement('div');
+  contributeContainer.className = "contributeContainer";
+  
+  const contributeHead = document.createElement('div');
+  contributeContainer.appendChild(contributeHead);
+  contributeHead.innerHTML = "NO CONTRIBUTION's NEEDED";
+  contributeHead.className = "contributeHead";
+
+  const contributeBody = document.createElement('div');
+  contributeContainer.appendChild(contributeBody);
+  contributeBody.className = "contributeBody";
+  contributeBody.innerHTML = "I am Glad that my Site allowed you to save yourself some time!";
+
+  return  contributeContainer;
+}
--- src/script.js
+++ src/script.js
@@ -2,16 +2,31 @@ import './style.css';
 import abouticon from './assets/icons/about-icon.svg';
 import { homeBox } from './modules/domController.js';
 import redDesign from './assets/images/red-design.jpg'
+import {contributeBox} from './modules/domController.js';
+
 
 const aboutIconImg = document.getElementById("aboutIcon");
 aboutIconImg.src = abouticon;
 
+const btn = document.querySelectorAll("button");
+
 const centerContent = document.getElementById("center");
 
-if(centerContent){
-    centerContent.appendChild(homeBox());
-}
  centerContent.style.backgroundImage = `url(${redDesign})`;
  centerContent.style.backgroundSize = "cover";
  centerContent.style.backgroundPosition = "center";
  centerContent.style.backgroundRepeat = "no-repeat";
+ centerContent.appendChild(homeBox());
+
+btn.forEach(button => {
+    button.addEventListener('click', () => {
+        if(button.textContent === "Home"){
+            centerContent.innerHTML = "";
+            centerContent.appendChild(homeBox());
+        }
+        else if(button.textContent === "Contribute"){
+            centerContent.innerHTML = "";
+            centerContent.appendChild(contributeBox());
+        }
+    })
+})
