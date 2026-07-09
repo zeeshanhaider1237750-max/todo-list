a65a9471e550704d3e251b034db64becc8214953
Zeeshan Haider <ZeeshanHaider.1237750@gmail.com>
2026-07-09 14:47:32 +0500

    central portion

--- src/script.js
+++ src/script.js
@@ -1,5 +1,17 @@
 import './style.css';
 import abouticon from './assets/icons/about-icon.svg';
+import { homeBox } from './modules/domController.js';
+import redDesign from './assets/images/red-design.jpg'
 
 const aboutIconImg = document.getElementById("aboutIcon");
 aboutIconImg.src = abouticon;
+
+const centerContent = document.getElementById("center");
+
+if(centerContent){
+    centerContent.appendChild(homeBox());
+}
+ centerContent.style.backgroundImage = `url(${redDesign})`;
+ centerContent.style.backgroundSize = "cover";
+ centerContent.style.backgroundPosition = "center";
+ centerContent.style.backgroundRepeat = "no-repeat";
