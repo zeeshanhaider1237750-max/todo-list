87900678ef6c14ff478f359a465837e889025738
Zeeshan Haider <ZeeshanHaider.1237750@gmail.com>
2026-07-11 00:52:03 +0500

    Project.js compilation

--- src/modules/project.js
+++ src/modules/project.js
@@ -1,4 +1,9 @@
+import { sr } from "date-fns/locale";
 import "../pages/project_page/projectView.css";
+import { isThisSecond } from "date-fns";
+import { remove } from "lodash";
+
+let todoListLibrary = [];
 
 export const projectBox = () => {
   const projectContainer = document.createElement("div");
@@ -20,6 +25,7 @@ export const projectBox = () => {
 
   return projectContainer;
 };
+
 const projectBodyBox = () => {
   const box = document.createElement("div");
 
@@ -31,10 +37,72 @@ const projectBodyBox = () => {
   header.appendChild(butn);
   butn.className = "butn";
   butn.setAttribute("data-hover-text", "Add a Todo List");
+  butn.addEventListener('click', () => {
+    let serialNumber = todoListLibrary.length + 1;
+    let arrowIcon = "▶";
+
+    let nameInputField = document.createElement('input');
+    nameInputField.className = "nameInputField";
+
+    let newTodo = todoFactory(serialNumber, arrowIcon, nameInputField);
+
+    todoListLibrary.push(newTodo);
+
+    let newHtmlRow = todoDom(newTodo);
+
+    body.appendChild(newHtmlRow);
+  }) 
   
   const body = document.createElement("div");
   body.className = "body";
   box.appendChild(body);
-  // body.appendChild = ;
+  
   return box;
 };
+
+let todoFactory = (serialNo, arrow, name) => {
+  return {serialNo, arrow, name};
+}
+
+let todoDom = (todo) => {
+  let todoRow = document.createElement('div');
+  todoRow.className = "todoRow";
+
+  let serial = document.createElement('div');
+  todoRow.appendChild(serial);
+  serial.textContent = todo.serialNo;
+  serial.className = "serial";
+
+  let arrow = document.createElement('div');
+  todoRow.appendChild(arrow);
+  arrow.textContent = todo.arrow;
+  arrow.className = "arrow";
+
+  let inputName = document.createElement('input');
+  todoRow.appendChild(inputName);
+  inputName.textContent = todo.name;
+  inputName.className = "inputName";
+  inputName.placeholder = "Input the Name of Your Todo and press Enter";
+  inputName.setAttribute('required', '');
+  inputName.setAttribute('maxlength', '50');
+
+  let checkBox = document.createElement('div');
+  todoRow.appendChild(checkBox);
+  checkBox.className = "checkBox";
+
+  let removeButton = document.createElement('div');
+  todoRow.appendChild(removeButton);
+  removeButton.className = "removeButton";
+  removeButton.textContent = "Remove todo";
+
+  checkBox.addEventListener('click', () => {
+    if(checkBox.innerHTML === "" || checkBox.innerHTML === "✖"){
+      checkBox.innerHTML = "✔";
+    }
+    else if(checkBox.innerHTML === "✔"){
+      checkBox.innerHTML = "✖"
+    }
+  })
+
+  return todoRow;
+}
